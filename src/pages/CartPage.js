c2b1c3a920b86842f5c1d4fa7ee2cc5cdb1d05a0
amanuelcm27 <amanuelfirew27@gmail.com>
2024-02-26 03:02:28 +0300

    cart-page-empty-state

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -2,23 +2,26 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 export const CartPage = () => {
   const [items, setItems] = useState([]);
+  const [cartState, setCartState] = useState(false);
   let total = 0;
   let sum = 0;
-  items.map((item)=> {
+  items.map((item) => {
     const pricePerItem = item.quantity * item.price;
-    sum += pricePerItem 
-    total = Math.round(sum)
-  })
+    sum += pricePerItem;
+    total = Math.round(sum);
+  });
   useEffect(() => {
-    const storedAsString = localStorage.getItem("items") || {};
+    const storedAsString = localStorage.getItem("items") || "{}";
     const storedItemsObject = JSON.parse(storedAsString);
     const storedItemsArray = Object.values(storedItemsObject);
     const storedItems = Object.keys(storedItemsObject)
       .map((key) => storedItemsObject[key])
       .flat();
     setItems(storedItems);
+    if (storedAsString !== "{}") {
+      setCartState(true);
+    }
   }, []);
-  items.map((item) => console.log(item));
   return (
     <>
       <div className="cart-container">
@@ -30,31 +33,49 @@ export const CartPage = () => {
               continue shopping
             </Link>
           </p>
-          {items.map((item) => (
-            <div key={item.id} className="cart-item">
-              <img className="cart-item-image" src={item.image}></img>
+          {cartState ? (
+            items.map((item) => (
+              <div key={item.id} className="cart-item">
+                <img className="cart-item-image" src={item.image}></img>
+                <div className="cart-item-detail">
+                  <Link to={`/detail/${item.id}`}>
+                    <span className="cart-item-name">{item.name}</span>
+                  </Link>
+
+                  <span> Quantity : {item.quantity}</span>
+                  <span className="cart-item-price">${item.price}</span>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="cart-item">
+              <img
+                className="cart-item-image"
+                src="images/empty cart.png"
+              ></img>
               <div className="cart-item-detail">
-                <Link to={`/detail/${item.id}`}>
-                <span className="cart-item-name">{item.name}</span>
-                </Link>
-               
-                <span> Quantity : {item.quantity}</span>
-                <span className="cart-item-price">${item.price}</span>
+                <span className="cart-item-price"></span>
               </div>
             </div>
-          ))}
+          )}
         </div>
         <div className="order-container">
           <span className="cart-header">Order Summary</span>
+          
           {items.map((item) => (
             <div className="order-list">
               <div className="list-item">{item.name} </div>
-              <div className="list-item-price"> ${item.price * item.quantity}</div>
+              <div className="list-item-price">
+                {" "}
+                ${item.price * item.quantity}
+              </div>
             </div>
-            
           ))}
 
-          <p style={{fontSize:"1.5rem",marginTop:"50px"}}> Total:{total}</p>
+          <p style={{ fontSize: "1.5rem", marginTop: "50px" }}>
+            {" "}
+            Total:{total}
+          </p>
           <button className="checkout">Check Out</button>
         </div>
       </div>
